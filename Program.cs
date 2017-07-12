2eddf34d469813514b23dbfb8bcc0cbfc9677402
cleverbek1991 <dilshod@Dilshods-MacBook-Pro.local>
2017-07-12 14:07:26 -0500

    Final Version

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace linq
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             /* ======= Restriction/Filtering Operations ======= */
             // Find the words in the collection that start with the letter 'L'
@@ -77,6 +77,85 @@ namespace linq
             {
                 Console.WriteLine(s);
             }
+            
+            /* ======= Using Custom Types ======= */
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+                new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                new Bank(){ Name="Bank of America", Symbol="BOA"},
+                new Bank(){ Name="Citibank", Symbol="CITI"},
+            };
+
+            // ============== Final Version ================ 
+            /*
+                TASK:
+                As in the previous exercise, you're going to output the millionaires,
+                but you will also display the full name of the bank. You also need
+                to sort the millionaires' names, ascending by their LAST name.
+
+                Example output:
+                    Tina Fey at Citibank
+                    Joe Landy at Wells Fargo
+                    Sarah Ng at First Tennessee
+                    Les Paul at Wells Fargo
+                    Peg Vale at Bank of America
+            */
+            var grouped = customers.Where(c => c.Balance >= 1000000)
+                .GroupBy(d => d.Bank);
+
+            foreach (var item in grouped)
+            {
+                Console.WriteLine($"{item.Key} {item.Count()}");
+                foreach (var customer in item)
+                {
+                    Console.WriteLine($"{customer.Name} {customer.Balance}");
+                }
+            }
+            /*from customer in customers
+            join bank in banks on customer equals bank.Symbol into bankName
+            select new { bank = customer, b = bankName }; */
+
+            var millionaireReport = from c in customers
+                where c.Balance >= 1000000
+                orderby c.Name[c.Name.IndexOf(" ") + 1] ascending
+                join b in banks on c.Bank equals b.Symbol
+                select new {Bank = b.Name, Name = c.Name};
+
+
+            foreach (var customer in millionaireReport)
+            {
+                Console.WriteLine($"{customer.Name} at {customer.Bank}");
+            }
+
+        }
+        // Define a bank
+        public class Bank
+        {
+            public string Symbol { get; set; }
+            public string Name { get; set; }
+        }
+
+        // Define a customer
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
         }
     }
 }
